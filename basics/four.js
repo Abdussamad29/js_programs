66aac2eea60453f603ec773149e23bd903df128e
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2023-06-18 06:04:25 +0000

    conversion of dataTypes

---
+++ basics/four.js
@@ -0,0 +1,11 @@
+let j = "29";
+console.log(j);
+console.log(typeof j);
+console.log(typeof (j));
+
+let value = Number(j)
+console.log(j);
+console.log(typeof value);
+// '29' => 29(number)
+//'29abc' => NaN
+// true => 1, false => 0
