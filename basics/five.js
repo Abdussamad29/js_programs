f9c63304c7a524ddf5c81756ec1fb2547f6b720a
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2023-06-19 11:19:39 +0000

    data-Types

---
+++ basics/five.js
@@ -0,0 +1,15 @@
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("2"+ 4 + 2);
+console.log(2+3 + "2");
+
+// data-types
+//primitives - 7types nnbbsu
+const id = Symbol(1)
+const id2 = Symbol(1)
+console.log(id === id2);
+
+// refrence -non-primitives --> array,objects, functions
