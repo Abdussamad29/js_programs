0379ae35f877573cc688942d0365610ca1a6e94f
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2023-06-20 13:31:37 +0000

    methods added

---
+++ basics/string_methods.js
@@ -0,0 +1,13 @@
+"use strict"
+const Name = new String("dellvostro")
+console.log(Name.charAt(4)); // -> outputs v
+console.log(Name.indexOf('t')); // -> provides the index
+console.log(Name.charCodeAt(4));// -> gives the unicode value
+
+let m = "hi"
+let n = "there "
+console.log(m.concat(" ",n));  //-> joins the string
+console.log(m.toUpperCase());
+
+
+
