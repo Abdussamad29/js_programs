302b9e2b8606512e83df13b708849e12c2332b58
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2023-06-21 06:13:05 +0000

    numbers

---
+++ basics/numbers.js
@@ -0,0 +1,24 @@
+// const age = new Number(12)
+// console.log(age);
+// console.log(typeof age);
+// console.log(age.toString().includes(2));
+// console.log(age.toFixed(1));
+
+// const number = 1070.213646
+// console.log(number.toPrecision(6));
+
+// let value  =  100000
+// console.log(value.toLocaleString("en-IN"));
+
+// console.log(Math.abs(-5));
+// console.log(Math.ceil(5.9));
+// console.log(Math.floor(5.9));
+// console.log(Math.round(5.9));
+
+ const min = 10
+ const max = 16
+
+ console.log(Math.floor(Math.random()*(max-min+1)+min));//---> +1 to avoid zero
+
+
+
