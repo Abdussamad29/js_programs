189982afb6d13cd0cdc29146fbd00fc1243d28b3
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2023-06-21 13:25:12 +0000

    more code added

--- basics/numbers.js
+++ basics/numbers.js
@@ -1,24 +1,36 @@
-// const age = new Number(12)
-// console.log(age);
-// console.log(typeof age);
-// console.log(age.toString().includes(2));
-// console.log(age.toFixed(1));
+   const age = new Number(12)
+   console.log(age);
+   console.log(typeof age);
+   console.log(age.toString().includes(2));
+   console.log(age.toFixed(1));
 
-// const number = 1070.213646
-// console.log(number.toPrecision(6));
+   const number = 1070.213646
+   console.log(number.toPrecision(6));
 
-// let value  =  100000
-// console.log(value.toLocaleString("en-IN"));
+   let value  =  100000
+   console.log(value.toLocaleString("en-IN"));
 
-// console.log(Math.abs(-5));
-// console.log(Math.ceil(5.9));
-// console.log(Math.floor(5.9));
-// console.log(Math.round(5.9));
+   console.log(Math.abs(-5));
+   console.log(Math.ceil(5.9));
+   console.log(Math.floor(5.9));
+   console.log(Math.round(5.9));
 
- const min = 10
- const max = 16
+   const min = 10
+   const max = 16
 
- console.log(Math.floor(Math.random()*(max-min+1)+min));//---> +1 to avoid zero
+   console.log(Math.floor(Math.random()*(max-min+1)+min)); //---> +1 to avoid zero
+
+
+ const small = 1;
+ const big = 6;
+ console.log(Math.floor(Math.random()*(small-big+1)+small));
+
+ let number_2 = 1000700
+ console.log(number_2.toLocaleString("en-IN"));
+ 
+ console.log(number_2.toString().indexOf(7));
+ 
+
 
 
 
