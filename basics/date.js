0c1dd01985f8a736d6f131f0723b814bb34d3f42
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2023-06-22 06:23:28 +0000

    date object

---
+++ basics/date.js
@@ -0,0 +1,28 @@
+const date = new Date()
+console.log(date.toDateString());
+console.log(date.toJSON());
+console.log(date.toLocaleString());
+
+let data = new Date(2023,5,22,11,19)
+console.log(data.toDateString());
+console.log(data.toLocaleString());
+
+const owndate  = new Date("2023-06-22")
+console.log(owndate.toLocaleString());
+console.log(owndate.getTime());
+
+
+// let timeStamp = Date.now()
+// console.log(Math.floor(Date.now()/1000));
+
+let tarikh =  new Date()
+console.log(tarikh.getFullYear());
+console.log(tarikh.getDay());
+tarikh.toLocaleString('default',{ month:"narrow"})
+
+let m = {
+    name:'king',
+    age:29
+
+}
+console.log(m.name);
