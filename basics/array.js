6520704264e1601b8002167ad9ed2751c833a06a
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2023-07-03 18:27:37 +0000

    array

--- basics/array.js
+++ basics/array.js
@@ -4,3 +4,10 @@ console.log(m[2]);
 //checking if the variable is an array or not
 const arr = Array.isArray(m)
 console.log(arr);
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+let Arr = Array.of(score1,score2,score3);
+console.log(Arr);
