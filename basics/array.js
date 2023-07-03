2861bc3500e533bab51aa3bdf64dafb0ba130a9c
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2023-07-03 18:32:12 +0000

    array_functions

--- basics/array.js
+++ basics/array.js
@@ -11,3 +11,11 @@ let score3 = 300;
 
 let Arr = Array.of(score1,score2,score3);
 console.log(Arr);
+
+console.log(Array.from("Array"));
+
+let numbers = [12,45,67,5668,368,457]
+console.log(numbers.slice(1,3));
+
+let num = numbers.splice(1,3)
+console.log(num);
