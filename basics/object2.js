f54264981da0bc47ed71df3538aef691f42f8055
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2023-07-05 11:11:03 +0000

    object_properties

--- basics/object2.js
+++ basics/object2.js
@@ -14,3 +14,38 @@ let other_user = {
 
 let result = Object.assign({},user,other_user)
 console.log(result);
+
+//other way to print the above program
+
+let total = {...user,...other_user}
+console.log(total);
+
+//working with arrays
+
+const worker = [
+    {
+        id : '123acs',
+        designation:'janitor'
+    },
+    {
+        salary:'12k',
+        bonus:'2k'
+
+    }
+]
+
+console.log(worker[0].id);
+console.log(worker[0].designation);
+
+console.log(worker[1].salary);
+
+//some more object methods
+
+console.log(Object.keys(other_user));
+console.log(Object.values(other_user));
+console.log(Object.entries(other_user));
+
+// to check the existance og the property in the object
+
+console.log(other_user.hasOwnProperty('name2'));
+console.log(user.hasOwnProperty('name'));
