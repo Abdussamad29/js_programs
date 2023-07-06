ea1ccb0f8659cc4ed212ea5bce4505ee487559a8
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2023-07-06 15:33:25 +0000

    commmit

---
+++ basics/functions.js
@@ -0,0 +1,12 @@
+function addTwoNumbers(num1,num2){
+    return num1 + num2
+}
+console.log(addTwoNumbers(20,9));
+
+// other way 
+
+function twoNumbers(n1,n2) {
+    console.log(n1*n2);
+    
+}
+twoNumbers(12,4)
